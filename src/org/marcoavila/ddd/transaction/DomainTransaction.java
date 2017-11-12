971d63ccd8e180364b20ce3151ff2c60a0884e89
Marco Avila <marcoavilasz@yahoo.com.br>
2017-11-12 11:11:46 -0200

    Update DomainTransaction.java

--- src/org/marcoavila/ddd/transaction/DomainTransaction.java
+++ src/org/marcoavila/ddd/transaction/DomainTransaction.java
@@ -3,6 +3,10 @@ package org.marcoavila.ddd.transaction;
 import org.marcoavila.ddd.Entity;
 import org.marcoavila.ddd.facade.BaseReturn;
 
+/**
+ * 
+ * @author Marco Avila
+ */
 public interface DomainTransaction<AGGREGATE extends Entity<?>> {
 
 	public BaseReturn<AGGREGATE> execute(AGGREGATE rootEntity);
