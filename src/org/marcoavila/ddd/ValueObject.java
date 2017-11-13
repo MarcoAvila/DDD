f78004d8ffca68b5e2dd8c2f51effd960c3094e5
Marco Avila <marcoavilasz@yahoo.com.br>
2017-11-13 16:18:02 -0200

    Update ValueObject.java

--- src/org/marcoavila/ddd/ValueObject.java
+++ src/org/marcoavila/ddd/ValueObject.java
@@ -2,6 +2,11 @@ package org.marcoavila.ddd;
 
 import java.io.Serializable;
 
+/**
+ * Abstraction that represents a Domain Value Object. 
+ *
+ * @author Marco Avila
+ */
 public interface ValueObject extends Serializable {
 
 }
