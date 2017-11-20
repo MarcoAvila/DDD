95f8359f3e785bbf9579a411dcd9157935cbd5b3
Marco Avila <marcoavilasz@yahoo.com.br>
2017-11-20 12:01:33 -0200

    Update ValueObject.java

--- src/org/marcoavila/ddd/ValueObject.java
+++ src/org/marcoavila/ddd/ValueObject.java
@@ -3,6 +3,8 @@ package org.marcoavila.ddd;
 /**
  * Abstraction that represents an Domain Value Object. 
  *
+ * Enforces the equality verification and enable invariants checking.
+ *
  * @author Marco Avila
  */
 public interface ValueObject<VO> extends InvariantsOwner {
